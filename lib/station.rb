e44864a0bfaf372d2b42efe2b61cd053130801c5
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-11 17:01:43 +0100

    added station_zone varibale and attr_accessor, passes tests

--- lib/station.rb
+++ lib/station.rb
@@ -1,6 +1,8 @@
 class Station
   attr_accessor :station_name
-  def initialize(name = 'no_name')
+  attr_accessor :station_zone
+  def initialize(name = 'no_name', zone = 0)
     @station_name = name
+    @station_zone = zone
   end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -4,7 +4,10 @@ describe Station do
   it 'checks if class station exists' do
     expect(subject).to be_instance_of Station
   end
-  it 'responds to name with 1 argument' do
+  it 'responds to station_name' do
     expect(subject).to respond_to(:station_name)
+  end
+  it 'responds to station_zone' do
+    expect(subject).to respond_to(:station_zone)
   end
 end
