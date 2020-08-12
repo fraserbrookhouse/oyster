beafdc597f04438b5efef21cbe1711ee18fa0158
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-12 17:20:33 +0100

    added tests for setting station name and zone

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -10,4 +10,12 @@ describe Station do
   it 'responds to station_zone' do
     expect(subject).to respond_to(:station_zone)
   end
+  it 'sets a station name' do
+    station = Station.new('Euston')
+    expect(station.station_name).to eq 'Euston'
+  end
+  it 'sets a station zone' do
+    station = Station.new('Euston', 3)
+    expect(station.station_zone).to eq 3
+  end
 end
